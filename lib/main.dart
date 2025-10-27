c27cd91b5cf01b8febf921d1c698818f71d96987
najmabashiir <najaxbashiirali>
2025-10-27 15:06:01 +0300

    learn more containers as column
    Signed-off-by: najmabashiir <najaxbashiirali>

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,57 @@ class myapp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body:SafeArea(
-          child: Container(
-            width: 200.0,
-            height: 200.0,
-            // margin: EdgeInsets.all(200),
-            // margin: EdgeInsets.only(top: 100,left: 200),
-            // margin: EdgeInsets.symmetric(horizontal: 200,vertical: 200) ,
-            margin: EdgeInsets.fromLTRB(100,100,100,100),
-            // padding: EdgeInsets.all(50),
-            // padding: EdgeInsets.symmetric(vertical: 12,horizontal: 30),
-            // padding: EdgeInsets.only(top: 30 ,left: 30),
-            padding: EdgeInsets.fromLTRB(60, 30, 60, 30),
-            color: Colors.white,
+          child: Column(
+            children: [
+              Container(
+                width: 200.0,
+                height: 200.0,
+                // margin: EdgeInsets.all(200),
+                // margin: EdgeInsets.only(top: 100,left: 200),
+                // margin: EdgeInsets.symmetric(horizontal: 200,vertical: 200) ,
+                // margin: EdgeInsets.fromLTRB(100,100,100,100),
+                // padding: EdgeInsets.all(50),
+                // padding: EdgeInsets.symmetric(vertical: 12,horizontal: 30),
+                // padding: EdgeInsets.only(top: 30 ,left: 30),
+                padding: EdgeInsets.fromLTRB(60, 30, 60, 30),
+                color: Colors.white,
 
-            child: Text("hi najma",),
+                child: Text("hi najma",),
+              ),
+              Container(
+                width: 200.0,
+                height: 200.0,
+                // margin: EdgeInsets.all(200),
+                // margin: EdgeInsets.only(top: 100,left: 200),
+                // margin: EdgeInsets.symmetric(horizontal: 200,vertical: 200) ,
+                // margin: EdgeInsets.fromLTRB(100,100,100,100),
+                // padding: EdgeInsets.all(50),
+                // padding: EdgeInsets.symmetric(vertical: 12,horizontal: 30),
+                // padding: EdgeInsets.only(top: 30 ,left: 30),
+                padding: EdgeInsets.fromLTRB(60, 30, 60, 30),
+                color: Colors.blue,
+
+                child: Text("hi najma",),
+              ),
+              Container(
+                width: 200.0,
+                height: 200.0,
+                // margin: EdgeInsets.all(200),
+                // margin: EdgeInsets.only(top: 50,),
+                // margin: EdgeInsets.symmetric(horizontal: 200,vertical: 200) ,
+                // margin: EdgeInsets.fromLTRB(100,100,100,100),
+                // padding: EdgeInsets.all(50),
+                // padding: EdgeInsets.symmetric(vertical: 12,horizontal: 30),
+                // padding: EdgeInsets.only(top: 30 ,left: 30),
+                padding: EdgeInsets.fromLTRB(60, 30, 60, 30),
+                color: Colors.red,
+
+                child: Text("hi najma",),
+              ),
+
+
+
+            ],
           ),
         ),
       ),
