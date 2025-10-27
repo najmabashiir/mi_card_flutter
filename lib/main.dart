48f1b87e3ac9de4080374f4ee8d67bb0e2fa908c
najmabashiir <najaxbashiirali>
2025-10-27 14:23:33 +0300

    commit
    Signed-off-by: najmabashiir <najaxbashiirali>

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myapp());
+}
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body:SafeArea(
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            // margin: EdgeInsets.all(200),
+            // margin: EdgeInsets.only(top: 100,left: 200),
+            margin: ,
+            color: Colors.white,
+            child: Text("hi najma"),
+          ),
+        ),
+      ),
+    );
+  }
+}
